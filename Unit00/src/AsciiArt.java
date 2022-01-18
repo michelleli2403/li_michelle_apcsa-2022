b0b78b63991ff1bb31d1966642ad6402145084fb
michelleli2403 <97621458+michelleli2403@users.noreply.github.com>
2022-01-18 08:04:37 -0800

    Update AsciiArt.java

--- Unit00/src/AsciiArt.java
+++ Unit00/src/AsciiArt.java
@@ -1,23 +1,29 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Michelle Li
+//Date - 1/16/22
+//Class - period 1
+//Lab  - Ascii Art
 
 public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
-		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("Michelle Li \n\n" );
+		System.out.println("Penguin" );
 		System.out.println("\n\n\n\n" );
 
-		System.out.println("                /\\                " );
-		System.out.println("               /  \\               " );
-		System.out.println("              /    \\              " );
-		System.out.println("             [------]             " );
+		System.out.println("                 ######       ");
+		System.out.println("               ##      ##     ");
+		System.out.println("              ##  ^   ^ ##    ");
+		System.out.println("             ##     \\/   ##  ");
+		System.out.println("           ####          #### ");
+		System.out.println("             ##          ##   ");
+		System.out.println("              ##        ##    ");
+		System.out.println("               ##########     ");
+		System.out.println("                 ##   ##      ");
+
 		//add other output
 
 		System.out.println(" \n\n\n\nHelpFul Hints" );
