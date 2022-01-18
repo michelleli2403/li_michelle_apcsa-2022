68ffcfb4462662293e840fee05028d6ee769a729
michelleli2403 <michelleli12315@gmail.com>
2022-01-17 17:24:39 -0800

    unit0

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,63 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+//Name - Michelle Li
+//Date - 1/16/22
+//Class - period 1
+//Lab  - Ascii Art 
+=======
+=======
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+//Name -
+//Date -
+//Class -
+//Lab  -
+<<<<<<< HEAD
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+=======
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+<<<<<<< HEAD
+<<<<<<< HEAD
+		System.out.println("Michelle Li \n\n" );
+		System.out.println("Penguin" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("                 ######       ");
+		System.out.println("               ##      ##     ");
+		System.out.println("              ##  ^   ^ ##    ");
+		System.out.println("             ##     \\/   ##  ");
+		System.out.println("           ####          #### ");
+		System.out.println("             ##          ##   ");
+		System.out.println("              ##        ##    ");
+		System.out.println("               ##########     ");
+		System.out.println("                 ##   ##      ");
+=======
+=======
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+		System.out.println("Your Name \n\n" );
+		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("                /\\                " );
+		System.out.println("               /  \\               " );
+		System.out.println("              /    \\              " );
+		System.out.println("             [------]             " );
+<<<<<<< HEAD
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+=======
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,62 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+//Name - Michelle Li
+//Date - 1/17/22
+//Class - Period 1
+//Lab  - Ascii Box
+=======
+=======
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+//Name -
+//Date -
+//Class -
+//Lab  -
+<<<<<<< HEAD
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+=======
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+<<<<<<< HEAD
+<<<<<<< HEAD
+		System.out.println("Michelle Li \t  1/17/22 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+
+	}
+}
+=======
+=======
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+		System.out.println("name \t  date \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+<<<<<<< HEAD
+}
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
+=======
+}
+>>>>>>> 0a6015de497ddc1bc54645304100191f0e210cd9
