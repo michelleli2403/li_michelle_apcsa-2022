729a157462102cec6aa2182909d7f36c43df0faf
michelleli2403 <97621458+michelleli2403@users.noreply.github.com>
2022-01-26 09:58:51 -0800

    test

--- test/src/Test.java
+++ test/src/Test.java
@@ -2,5 +2,14 @@ import static java.lang.System.*;
 import static java.lang.Math.*;
 
 public class Test {
-	int x;
+	public static void main(String[] args) {
+		int a = 2;
+		if (a<3)
+			System.out.println("hello");
+		else {
+			System.out.println("hey");
+		}
+			System.out.println("hi");
+	}
+	
 }
---
+++ test/src/Triangle.java
@@ -0,0 +1,66 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import java.lang.Math.*;
+
+public class Triangle
+{
+	private int sideA, sideB, sideC;
+	private double perimeter;
+	private double theArea;
+
+	public Triangle()
+	{
+	   setSides(0,0,0);
+	   perimeter=0;
+	   theArea=0;
+	}
+
+	public Triangle(int a, int b, int c)
+	{
+		sideA=a;
+		sideB=b;
+		sideC=c;
+
+	}
+
+
+	public void setSides(int a, int b, int c)
+	{
+		sideA=a;
+		sideB=b;
+		sideC=c;
+
+	}
+
+	public void calcPerimeter( )
+	{
+		perimeter = sideA + sideB + sideC;
+	}
+
+	public void calcArea( )
+	{
+		double s;
+		s=perimeter/2;
+		theArea=Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));
+
+	}
+
+	public void print( )
+	{
+		System.out.println("\n\nThe area is :: "+theArea);
+	}
+	
+	//create a print method or toString or both
+	
+	public String toString()
+	{
+		return "" + theArea;
+	}
+}
