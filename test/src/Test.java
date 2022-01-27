d1eb19946efe27074b635397ed3508d4be7857df
michelleli2403 <michelleli12315@gmail.com>
2022-01-26 21:44:24 -0800

    unit05

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,13 +3,13 @@ import static java.lang.Math.*;
 
 public class Test {
 	public static void main(String[] args) {
-		int a = 2;
-		if (a<3)
-			System.out.println("hello");
-		else {
-			System.out.println("hey");
-		}
-			System.out.println("hi");
-	}
+		String s = "abcdef1xyzabf1";
+		//s = s.replaceAll("1", "#");
+		out.println(s.replaceAll("1", "#"));
+		out.println(s);
+
+
+
 	
+}
 }
