5fd21d8007929cefffcb462637010f6bed7bf4af
michelleli2403 <michelleli12315@gmail.com>
2022-02-03 07:34:45 -0800

    Update Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -4,8 +4,14 @@ import static java.lang.Math.*;
 public class Test
 {
 	public static void main(String[] args) {
-		String s = "a";
-		System.out.println(s.substring(0,-1));
+		int var1 = 0;
+		int var2 = 2;
+
+		while (((var1 / var2) >= 0)&&(var2 != 0))
+		{
+		   var1 = var1 + 1;
+		   var2 = var2 -1;
+		}
 
 
 	}
