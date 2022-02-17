154de2b37110e2479c8ee2e63a56fb55117cd909
michelleli2403 <97621458+michelleli2403@users.noreply.github.com>
2022-02-17 08:41:32 -0800

    Update Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,9 +1,17 @@
-import static java.lang.System.*;
-import java.util.Scanner;
-import static java.lang.Math.*;
+import java.util.ArrayList;
 
 public class Test
 {
 	public static void main(String[] args) {
+		ArrayList<String> vals;
+		vals=new ArrayList<String>();
+		vals.add("aplus");
+		vals.add("rocks");
+		vals.add(0,"comp");
+		vals.add(1,"sci");
+		vals.add(1,"h");
+		vals.add(0,"m");
+		vals.add("hi");
+		System.out.println(vals);
 	}
 }
