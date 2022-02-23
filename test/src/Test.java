c577d538fce7f46836a45f122fdc0789fe707c93
michelleli2403 <michelleli12315@gmail.com>
2022-02-22 17:52:33 -0800

    unit9

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,26 +1,16 @@
-
+import java.util.Scanner;
 
 public class Test
 {
 	public static void main(String[] args) {
-		/*ArrayList<Integer> ray;
-		ray=new ArrayList<Integer>();
-		ray.add(23);
-		ray.add(11);
-		ray.set(1, 5);
-		ray.add(4);
-		ray.set(0, 7);
-		ray.add(53);
-		ray.set(1, 93);
-		ray.add(0,3);
-		ray.set(0, 99);
-		ray.add(0,33);
-		System.out.println(ray);*/
-		
-		/*List<Integer> list = new ArrayList<>();
-		list.add(3);
-		System.out.println(list.get(1));*/
-		
+		String nums = "1 2 3 4 5 6 7";
+		Scanner chopAh = new Scanner(nums);
+		while(chopAh.hasNextDouble())
+		{ 
+		   System.out.println(chopAh.nextDouble()%2);
 		}
+
+	}
 		
 }
+
