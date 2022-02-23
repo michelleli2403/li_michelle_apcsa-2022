b576523f5e0ad818d1d8b833a3738e3cb12817d1
michelleli2403 <michelleli12315@gmail.com>
2022-02-23 09:01:18 -0800

    Update Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,20 +3,9 @@ import java.util.Scanner;
 public class Test
 {
 	public static void main(String[] args) {
-		int n = 1;
-		int m=1;
-		do {
-			System.out.println(n+"n");
-			n++;
-			System.out.println(n);
-		}while (n<3);
-		
-		while (m<3) {
-			System.out.println(m+"mmmm");
-			m++;
-			System.out.println(m+"m");
-			
-		}
+		int x=0;
+		System.out.print( x++ );
+		System.out.println(++x);
 	}
 		
 }
