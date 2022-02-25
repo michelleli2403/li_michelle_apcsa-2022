351fbd95874f8ffead5d01b09bdde2666cc24627
michelleli2403 <97621458+michelleli2403@users.noreply.github.com>
2022-02-25 09:26:17 -0800

    unit11

--- Unit11/src/ToyRunner.java
+++ Unit11/src/ToyRunner.java
@@ -8,5 +8,11 @@ public class ToyRunner
 {
 	public static void main(String[] args)
 	{
+		Toy t =  new Toy("sorry");
+		System.out.println(t);
+		
+		t=new Toy("ji goe");
+		t.setCount(5);
+		System.out.println(t);
 	}
 }
