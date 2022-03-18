f9ffd8597e1d898aa00305a96bb5fea76db2f42c
michelleli2403 <michelleli12315@gmail.com>
2022-03-17 23:29:19 -0700

    Update Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,26 +1,19 @@
 public class Test {
-	public TokenPass(int playerCount) {
-		board = new int[playerCount];
-		for (int i=0; i<board.length; i++) {
-			board[i]=((int)Math.random()*10+1);
+	public static void main(String[] args) {
+		int[] a1 = new int[10];
+		int[] a2 = new int[4];
+		for (int i=0; i<a1.length; i++) {
+			a1[i]=i;
 		}
-		currentPlayer = ((int) Math.random() * playerCount);
-	}
-	
-	public void distributeCurrentPlayerTokens() {
-		int i=currentPlayer;
-		while (board[currentPlayer]!=0) {
-			if (i==board.length-1) {
-				i=0;
-				board[i]+=1;
-			}
-			else {
-				board[i+1]+=1;
-			}
-			board[currentPlayer]--;
-			i++;
+		for (int j=0; j<a2.length; j++) {
+			a2[j]=j+3;
 		}
+		System.out.println(a1.length);
+		a1=a2;
 		
+		System.out.println(a1.length);
+		for (int k=0; k<a1.length; k++) {
+			System.out.println(a1[k]+" ffff");
+		}
 	}
-	
 }
