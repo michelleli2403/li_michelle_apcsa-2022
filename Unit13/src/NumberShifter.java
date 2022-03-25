291f2032a147b8109d338c7449fd82a0479684de
michelleli2403 <97621458+michelleli2403@users.noreply.github.com>
2022-03-25 09:28:08 -0700

    13

--- Unit13/src/NumberShifter.java
+++ Unit13/src/NumberShifter.java
@@ -12,9 +12,22 @@ public class NumberShifter
 {
 	public static int[] makeLucky7Array( int size)
 	{
-		return null;
+		int[] randomArray = new int[size];
+		for (int i=0; i<randomArray.length; i++) {
+			randomArray[i]=(int) (Math.random()*10+1);
+		}
+		return randomArray;
 	}
 	public static void shiftEm(int[] array)
 	{
+		for (int i=0; i<array.length-1; i++) {
+			for (int j=i; j<array.length; j++) {
+				int temp=array[i];
+				if (array[j]==7) {
+					array[i]=7;
+					array[j]=temp;
+				}
+			}
+		}
 	}
 }
--- Unit13/src/NumberShifterRunner.java
+++ Unit13/src/NumberShifterRunner.java
@@ -2,17 +2,32 @@
 //www.apluscompsci.com
 //Name -
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.io.File;
 import java.io.IOException;
-import static java.lang.System.*;
+import java.util.Arrays;
 
 public class NumberShifterRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
 		//add test cases
+		
+		int[] test = NumberShifter.makeLucky7Array(20);
+		System.out.println(Arrays.toString(test));
+		NumberShifter.shiftEm(test);
+		System.out.println(Arrays.toString(test));
+		
+		System.out.println();
+		int[] test2 = NumberShifter.makeLucky7Array(20);
+		System.out.println(Arrays.toString(test2));
+		NumberShifter.shiftEm(test2);
+		System.out.println(Arrays.toString(test2));
+		
+		System.out.println();
+		int[] test3 = NumberShifter.makeLucky7Array(20);
+		System.out.println(Arrays.toString(test3));
+		NumberShifter.shiftEm(test3);
+		System.out.println(Arrays.toString(test3));
+		
 	}
 }
 
