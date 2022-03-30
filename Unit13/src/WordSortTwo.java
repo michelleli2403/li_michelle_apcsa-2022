02b8c69d65bf70ede9d3c85b9e35d9e0d7b34de9
michelleli2403 <michelleli12315@gmail.com>
2022-03-29 21:06:43 -0700

    13

--- Unit13/src/WordSortTwo.java
+++ Unit13/src/WordSortTwo.java
@@ -12,15 +12,33 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		for (int i=0; i<sentence.length(); i++) {
+			wordRay=sentence.split(" ");
+		}
 	}
 
 	public void sort()
 	{
+		for (int i=0; i<wordRay.length-1; i++) {
+			int minIndex=i;
+			for (int j=i+1; j<wordRay.length; j++) {
+				if (wordRay[j].compareTo(wordRay[minIndex])<0) {
+					minIndex=j;
+				}
+			}
+			String temp=wordRay[i];
+			wordRay[i] = wordRay[minIndex];
+			wordRay[minIndex]=temp;
+			
+		}
 	}
 
 	public String toString()
 	{
 		String output="";
+		for (int i=0; i<wordRay.length; i++) {
+			output+=wordRay[i]+"\n";
+		}
 		return output+"\n\n";
 	}
 }
