fb6dc8870dd071d06efeb3737a4be756ed14582d
michelleli2403 <michelleli12315@gmail.com>
2022-04-17 21:23:11 -0700

    Update Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 public class Test {
 	
 	public static void main(String[] args) {
-		int[][] ticketInfo = { {25,20,25}, {25,20,25}, {25,2,2,2} };
-		String[][] seatingInfo = { {"Jamal", "Maria"}, {"Jake", "Suzy"}, {"Emma", "Luke"} };
-		System.out.println(Arrays.toString(seatingInfo[1]));
+		int one =8;
+		int two=7;
+		
+		System.out.println("8&7=="+(one&two));
 	}
 }
