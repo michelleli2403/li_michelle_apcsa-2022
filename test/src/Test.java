b5a59c9ef626cbf53790fe0497bd273096a6eac7
michelleli2403 <michelleli12315@gmail.com>
2022-04-24 23:14:47 -0700

    Update Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,9 +3,8 @@ import java.util.Arrays;
 public class Test {
 	
 	public static void main(String[] args) {
-		int one =8;
-		int two=7;
-		
-		System.out.println(one<<2);
+		int i=0;
+		i-=2;
+		System.out.println(i);
 	}
 }
