5d3a05e5b222411c3c5bea3b203023ba0b716cdb
michelleli2403 <michelleli12315@gmail.com>
2022-04-27 15:50:53 -0700

    Update Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class Test {
 	
 	public static void main(String[] args) {
-		String s = "abcd";
-		System.out.println(s.substring(4));
+		String s = "abcddd";
+		String t = "abcd  ";
+		System.out.println(s.compareTo(t));
 	}
 }
