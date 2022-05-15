47b6697b0bef109dc64e00b192cffde2d635ebf2
michelleli2403 <michelleli12315@gmail.com>
2022-05-15 15:57:05 -0700

    t

--- test/src/Test.java
+++ test/src/Test.java
@@ -2,9 +2,5 @@ import java.util.Arrays;
 
 public class Test {
 	
-	public static void main(String[] args) {
-		String s = "abcddd";
-		String t = "abcd  ";
-		System.out.println(s.compareTo(t));
-	}
+	
 }
--- test/src/TestRunner.java
+++ test/src/TestRunner.java
@@ -1,11 +1,12 @@
+import java.util.Arrays;
+
 public class TestRunner
 {
 	public static void main(String[] args) {
-		
-		String a = "aplus rocks";
-		a.replaceAll("s","-");
-		System.out.println(a);
-
+		int[][] a = {{0,1,2},{3,4,5}};
+		int[][] b = new int[2][3];
+		b=a;
+		System.out.println(Arrays.deepToString(b));
 	}
 }
 
