6e95155049c24678f341b2b63b53e0e375f2790d
michelleli2403 <michelleli12315@gmail.com>
2022-05-15 16:00:16 -0700

    Update TestRunner.java

--- test/src/TestRunner.java
+++ test/src/TestRunner.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TestRunner
 {
 	public static void main(String[] args) {
-		int[][] a = {{0,1,2},{3,4,5}};
+		ArrayList<Integer> a = new ArrayList();
 		int[][] b = new int[2][3];
-		b=a;
+		
 		System.out.println(Arrays.deepToString(b));
 	}
 }
